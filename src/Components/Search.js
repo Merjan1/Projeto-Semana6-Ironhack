f41e3a1f122e7e1fed5904fd2e46729d55624b0b
Jean <jvictor669@hotmail.com>
2021-02-17 16:44:22 -0300

    carousel e search bar funcionando

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import { Button, Form, FormControl} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Button, Form, FormControl } from 'react-bootstrap';
 import './Search.css'
 
-function Search() {
-    return (
-    <div className='d-flex justify-content-center m-auto'>
-        <Form inline>
-      <FormControl type="text" placeholder="Search Movie" className="input" />
-      <Button className='Button' variant="outline-success">Search</Button>
-    </Form>
+import axios from 'axios';
+
+class Search extends Component {
+  state = {
+    input: '',
+  }
+
+  handleSearch = (event) => {
+    const { value } = event.target;
+    this.setState({
+      input: value,
+    });
+  };
+
+  onClick = async (event) => {
+    try {
+      let url = `https://api.themoviedb.org/3/search/movie?api_key=b46a231393bf1d7236effd3142191445&query=${this.state.input}`
+      const response = await axios.get(url)
+      const movieId = response.data.results[0].id
+      window.location.href = `/movies/${movieId}`
+
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  render() {
+    return (<div className='d-flex justify-content-center m-auto' >
+      <Form inline>
+        <FormControl onChange={this.handleSearch} value={this.state.input} type="text" placeholder="Search Movie" className="input" id='search-bar' />
+        <Button onClick={this.onClick} className='Button' variant="outline-success">Search</Button>
+      </Form>
     </div>
     )
+  }
+
 }
 
 export default Search;

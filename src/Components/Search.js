c88f5f6b5edc12ee8de5ba7df588ab2d3129a1c5
edulp99@gmail.com <edulp99@gmail.com>
2021-02-17 17:13:14 -0300

    'att'

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -3,10 +3,27 @@ import { Button, Form, FormControl} from 'react-bootstrap';
 import './Search.css'
 
 function Search(props) {
+  function handleChange(event) {
+    props.setSearchMovie(event.currentTarget.value);
+  }
+
+  function onKeyDown(event) {
+    if (event.key === "Enter") {
+      handleSubmit(event);
+    }
+    return;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.history.push("/movies/:id");
+  }
+
     return (
     <div className='d-flex justify-content-center m-auto'>
-        <Form inline>
-      <FormControl type="text" placeholder="Search Movie" className="input" />
+        <Form inline onSubmit={handleSubmit}>
+      <FormControl type="text" placeholder="Search Movie" className="input" onKeyDown={onKeyDown} onChange={handleChange}
+          value={props.searchMovie} />
       <Button className='Button' variant="outline-success">Search</Button>
     </Form>
     </div>
